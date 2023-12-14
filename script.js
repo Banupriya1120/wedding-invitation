5234555feeeceb81b1030fe0c5545085f52d820e
Banupriya Palanisamy <banupriya.p1120@gmail.com>
2023-12-14 12:33:50 +0530

    made css changes

--- script.js
+++ script.js
@@ -16,23 +16,34 @@ const navbarLinks = document.querySelectorAll(".navbar-link");
 
 
 window.addEventListener("scroll",()=>{
-if (window.pageYOffset >= navbarOffsetTop){
-    navbar.classList.add("sticky");
-}else{
-    navbar.classList.remove("sticky");
+    mainFn();
+});
 
-}
- 
 
-sections.forEach((section,i) =>{
-    if (window.pageYOffset >= section.offsetTop - 10){
-        navbarLinks.forEach(navbarLink =>{
-            navbarLink.classList.remove("change");
-        });
-            navbarLinks[i].classList.add("change");
+
+const mainFn =() =>{
+    if (window.pageYOffset >= navbarOffsetTop){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    
     }
-});
-});
+     
+    
+    sections.forEach((section,i) =>{
+        if (window.pageYOffset >= section.offsetTop - 10){
+            navbarLinks.forEach(navbarLink =>{
+                navbarLink.classList.remove("change");
+            });
+                navbarLinks[i].classList.add("change");
+        }
+    });
+}
 
+mainFn();
+
+// window.addEventListener('resize', () =>{
+//     window.location.reload();
+// })
 
 
